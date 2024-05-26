aec86b974f771f23e018fec6db72f69c251fc0a2
Laura Murillas <laura.murillas@correounivalle.edu.co>
2024-05-26 11:58:30 -0500

    Script de pruebas automaticas vs2

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.core.urlresolvers import reverse
+from .models import OtpCode
 
 #Tests
 # Se crea una nueva instancia para testear los endpoints
@@ -9,7 +10,7 @@ from django.core.urlresolvers import reverse
 class UserAccountAPITest(APITestCase):
      
     def setUp(self):
-        self.client.post(reverse('user-account-add'), {'username': "fulanito", 'password': 123456789})
+        self.client.post(reverse('user-account-add'), {'id': 1, 'password': "123456789"})
 
 
     def test_get_info_user(self):
@@ -17,10 +18,23 @@ class UserAccountAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_add_info_user(self):
-        data = {'client': "sultanita", 'password': 123456789}
+        data = {'id': 1, 'password': "123456789"}
         response = self.client.post(reverse('info-user-add'), data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)   
 
-    def tearDown(self):
+    def test_update_info_user(self):
+        data = {'id': 1, 'password': "123456789"}
+        response = self.client.put(reverse('info-user-update', kwargs={'id': 1}), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(OtpCode.objects.get(id=1).quantity, 10)
+
+    def test_delete_info_user(self):
+        response = self.client.delete(reverse('info-user-delete', kwargs={'id': 1, 'item': 1}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) 
+
+   
+    #def tearDown(self):
         #Limpia la ejecución después de cada método de prueba.
-        pass
+    #    pass
+   
+
